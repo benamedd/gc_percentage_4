024b9dfcd7fa9e9db0a2d11852b1d7540172ada6
benamedd <benseti@yahoo.fr>
2025-08-09 22:16:13 +0100

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,69 @@
+ ## 2. service-worker.js
+```javascript
+const CACHE_NAME = 'dna-calculator-v1';
+const urlsToCache = [
+  '/',
+  '/static/css/',
+  '/static/js/',
+  'https://cdn.jsdelivr.net/npm/streamlit@1.28.1/',
+  'https://cdn.plot.ly/plotly-latest.min.js'
+];
+
+// Installation du Service Worker
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Cache ouvert');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Activation du Service Worker
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.map(function(cacheName) {
+          if (cacheName !== CACHE_NAME) {
+            console.log('Suppression du cache ancien:', cacheName);
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
+// Interception des requêtes réseau
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+
+        return fetch(event.request).then(
+          function(response) {
+            // Check if we received a valid response
+            if(!response || response.status !== 200 || response.type !== 'basic') {
+              return response;
+            }
+
+            var responseToCache = response.clone();
+
+            caches.open(CACHE_NAME)
+              .then(function(cache) {
+                cache.put(event.request, responseToCache);
+              });
+
+            return response;
+          }
+        );
+      }
+    )
+  );
+});
