baffe56c10353d946608a46fe516adcb4c6c8bf4
benamedd <benseti@yahoo.fr>
2025-08-13 11:42:26 +0100

    Update app.py

--- app.py
+++ app.py
@@ -337,7 +337,8 @@ if btn:
 
 # Footer / debug
 st.markdown("---")
-#st.markdown("Développé pour PWA — assurez-vous que `manifest.json`, `service-worker.js`, `icon-192.png` et `icon-512.png` sont à la racine du repo.")
+
 
 # End of file
+
 
