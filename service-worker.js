a895952e9bc156aa92856eef6ffef390a8b93182
benamedd <benseti@yahoo.fr>
2025-08-13 08:43:21 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,37 +1,34 @@
-const CACHE_NAME = 'bioinfo-cache-v1';
+const CACHE_NAME = 'bioinfo-cache-v2';
 const urlsToCache = [
-    './index.html',
-    './manifest.json',
-    './192x192.png',
-    './512x512.png'
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/192x192.png',
+  '/512x512.png',
+  '/1024x1024.png',
+  'https://gc-percentage-4-9.onrender.com'
 ];
 
 self.addEventListener('install', event => {
-    event.waitUntil(
-        caches.open(CACHE_NAME).then(cache => {
-            console.log('📦 Cache ouvert');
-            return cache.addAll(urlsToCache);
-        })
-    );
-    self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+  self.skipWaiting();
 });
 
 self.addEventListener('activate', event => {
-    event.waitUntil(
-        caches.keys().then(keys => Promise.all(
-            keys.map(key => {
-                if (key !== CACHE_NAME) {
-                    console.log('🗑 Suppression ancien cache :', key);
-                    return caches.delete(key);
-                }
-            })
-        ))
-    );
-    self.clients.claim();
+  event.waitUntil(
+    caches.keys().then(cacheNames =>
+      Promise.all(cacheNames.map(name => {
+        if(name !== CACHE_NAME) return caches.delete(name);
+      }))
+    )
+  );
+  self.clients.claim();
 });
 
 self.addEventListener('fetch', event => {
-    event.respondWith(
-        caches.match(event.request).then(response => response || fetch(event.request))
-    );
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
 });
