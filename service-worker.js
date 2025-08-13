248173abdabbfc6c87a7f707045d687668421ac8
benamedd <benseti@yahoo.fr>
2025-08-13 07:50:41 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,21 +1,37 @@
+const CACHE_NAME = 'bioinfo-cache-v1';
+const urlsToCache = [
+    './index.html',
+    './manifest.json',
+    './192x192.png',
+    './512x512.png'
+];
+
 self.addEventListener('install', event => {
     event.waitUntil(
-        caches.open('bioinfo-cache-v1').then(cache => {
-            return cache.addAll([
-                '/gc_percentage_4/',
-                '/gc_percentage_4/index.html',
-                '/gc_percentage_4/manifest.json',
-                '/gc_percentage_4/192x192.png',
-                '/gc_percentage_4/512x512.png'
-            ]);
+        caches.open(CACHE_NAME).then(cache => {
+            console.log('📦 Cache ouvert');
+            return cache.addAll(urlsToCache);
         })
     );
+    self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+    event.waitUntil(
+        caches.keys().then(keys => Promise.all(
+            keys.map(key => {
+                if (key !== CACHE_NAME) {
+                    console.log('🗑 Suppression ancien cache :', key);
+                    return caches.delete(key);
+                }
+            })
+        ))
+    );
+    self.clients.claim();
 });
 
 self.addEventListener('fetch', event => {
     event.respondWith(
-        caches.match(event.request).then(response => {
-            return response || fetch(event.request);
-        })
+        caches.match(event.request).then(response => response || fetch(event.request))
     );
 });
